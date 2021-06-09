a58078dffadaaf1b459be99dfd793d6db1c40ddf
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-06-09 13:42:45 -0700

    Update homework4.js

--- homework4.js
+++ homework4.js
@@ -7,3 +7,17 @@ for (let i = 0; i < number.length; i++){
     count++;
 }
     console.log (count);
+//  task2
+//  Count numbers of digit 9 in a number.
+
+let number = prompt("Enter a number")
+let count = 0;
+for (let i = 0; i < number.length; i++){
+    if (number[i] == 9){
+       count++; 
+    }
+}
+       console.log (count);
+// task3
+// Given two numbers. Print powers of 2 between that numbers. (without using Math.pow)
+
