895e331ac29636bdacc416cdab26b825fb5ed46c
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-06-10 00:56:03 +0500

    Add files via upload

---
+++ homework4.js
@@ -0,0 +1,9 @@
+// task1
+// Given a number print its digits count.
+
+let number = prompt("Enter a number")
+let count = 0;
+for (let i = 0; i < number.length; i++){
+    count++;
+}
+    console.log (count);
